e09b838e3b2ca8ee2aeda2f1d81f087ba1067051
Sumit Rana <sumitpanday125@gmail.com>
2023-10-29 04:03:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parent Healthy Diner")
+steamlit.header("My Dinner Menu")
+steamlit.text("Omega 3 & Blueberry Oatmeal")
+steamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Raged Egg") 
