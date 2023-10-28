428a135dccef38a05ab98adf398416df398ea288
Sumit Rana <sumitpanday125@gmail.com>
2023-10-29 04:18:25 +0530

    Update streamlit_app.py
    option to select fruits from give options

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,9 @@ streamlit.text("🐔 Hard-Boiled Free-Raged Egg")
 streamlit.text("🥑🍞 Avacado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# Lets  give option to pick fruit list, So that they can choose what fruit they wanted
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick Some Fruits", list(my_fruit_list.index))
+
+# Display fruit table on the page
 streamlit.dataframe(my_fruit_list)
