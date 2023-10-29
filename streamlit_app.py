4dd6696addde3c36d2e26b289800ceeff762dd5b
Sumit Rana <sumitpanday125@gmail.com>
2023-10-29 12:14:51 +0530

    Update streamlit_app.py
    fetch all rows from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchone() --> this is for to fetch only one row or singlar data
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
