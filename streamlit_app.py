85d76efdbcb6b2ba7f81596d4158a59041adcfec
Sumit Rana <sumitpanday125@gmail.com>
2023-10-29 09:27:15 +0530

    Update streamlit_app.py
    added new way response of API

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("🍓🍎🍒Fruityvice Fruit Advice!🍏🍐🍈")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # take the jason file data and shows into normalize form which shows in tabular format 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
